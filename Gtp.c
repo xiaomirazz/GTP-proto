102485e59d94ca9b28c2d4312360fdea5030ca41
xiaomirazz <zhangjiawei1989@hotmail.com>
2015-01-30 11:26:49 +0800

    day30

--- Gtp.c
+++ Gtp.c
@@ -45,3 +45,31 @@ SDS_UINT32 t3_scale_factor;
     gtpPacket.pnFlag = FALSE;                           \
     gtpPacket.msgType = GTP_MSG_TYPE_GPDU;              \
 }
+
+/*------------------------------------------------------------------------------------------------*/
+/*!
+  \macro          GTP_TX_NWK(a_buff,a_len ,a_destAddress,a_MSG_MORE_Flag)
+
+  \param[in]      a_buff : pointer to the encoded buffer.
+  \param[in]      a_len  : Buffer length.
+  \param[in]      a_destAddress : IP address of the destination.
+  \param[in]      a_MSG_MORE_Flag : Flag indicates if this packet is complete or its remaining part
+                                    will be sent next time.
+
+  \brief          Send the encoded GTP packet on the network.
+
+  \return         <Return Type>
+
+  \b See \b Also: <related functions, data structures, global variables>
+*/
+/*------------------------------------------------------------------------------------------------*/
+#define GTP_TX_NWK(a_buff,a_len ,a_destAddress,a_MSG_MORE_Flag,a_status_Ptr)                       \
+{                                                                                                  \
+    SDS_UINT32      peerHostIp;                                                                    \
+                                                                                                   \
+    peerHostIp = a_destAddress->addrV4.addr;                                                       \
+                                                                                                   \
+    LOG_TRACE_ARG("Sending Data Packet with length=%d", a_len, ZERO, ZERO);                        \
+    *a_status_Ptr = GTP_AL_sendGTPpacket(a_buff,a_len,peerHostIp,a_MSG_MORE_Flag);                 \
+                                                                                                   \
+}
